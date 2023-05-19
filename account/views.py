11dd9a5180d04f3a32a7b95b546982c4187f2e97
Flowmikro <cloplop091@gmail.com>
2023-05-19 22:54:56 +0300

    v1

---
+++ account/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+
+def user_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Аутентификация прошла успешно')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Неверный логин')
+    return render(request, 'account/login.html', {'form': form})
+
