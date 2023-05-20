2b02e53db19fb0d9333f1ea43775d5cd94b635ab
Flowmikro <cloplop091@gmail.com>
2023-05-20 21:55:40 +0300

    v1.2

--- account/views.py
+++ account/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
-def user_login(request):
-    form = LoginForm()
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Аутентификация прошла успешно')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Неверный логин')
-    return render(request, 'account/login.html', {'form': form})
+        user_form = UserRegisterForm()
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegisterForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
+
+
+# def user_login(request):
+#     form = LoginForm()
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request,
+#                                 username=cd['username'],
+#                                 password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Аутентификация прошла успешно')
+#                 else:
+#                     return HttpResponse('Disabled account')
+#             else:
+#                 return HttpResponse('Неверный логин')
+#     return render(request, 'account/login.html', {'form': form})
 
